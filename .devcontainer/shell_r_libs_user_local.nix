49d92b6d8c0d272de97a2632293487fbf5a5e880
John Gavin <john.b.gavin@gmail.com>
2024-03-09 20:38:53 +0000

    dont remove local packages that are subdirectories (set via R_LIBS_USER) from .libPaths()

--- .devcontainer/shell_r_libs_user_local.nix
+++ .devcontainer/shell_r_libs_user_local.nix
@@ -1,12 +1,14 @@
 # https://churchman.nl/tag/r/
-# nix-shell --pure shell_churchman.nix
+#   export "R_LIBS_USER"="./_r_libs_user"  
+#   Sys.setenv("R_LIBS_USER" = "./_r_libs_user") 
+# nix-shell --pure shell_r_libs_user_local.nix
 
 with import <nixpkgs> {};
 let
   my-r = rWrapper.override {
     packages = with rPackages; [ 
       ggplot2
-      plyr
+      dplyr
       tidyr
       devtools
     ];
@@ -15,11 +17,17 @@ in
   pkgs.mkShell {
     buildInputs = [
       bashInteractive
+      oh-my-zsh
       my-r
     ];
+    # NB: shell hook to create local user lib
+    #   export "R_LIBS_USER"="./_r_libs_user" or 
+    #   Sys.setenv("R_LIBS_USER" = "./_r_libs_user") 
     shellHook = ''
-      mkdir -p "$(pwd)/_libs"
-      export R_LIBS_USER="$(pwd)/_libs"
+      mkdir -p "$(pwd)/_r_libs_user"
+      export R_LIBS_USER="$(pwd)/_r_libs_user"
     '';
+    # Install local lib
+    # Sys.getenv("R_LIBS_USER") ; install.packages("rix", repos = c("https://b-rodrigues.r-universe.dev", "https://cloud.r-project.org"))
   }
 
