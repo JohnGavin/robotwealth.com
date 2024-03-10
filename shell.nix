a892ecd983cb42e86c2131e26cad1a3ee53dd993
John Gavin <john.b.gavin@gmail.com>
2024-03-10 15:50:53 +0000

    https://github.com/xtruder/nix-devcontainer

---
+++ shell.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell {
+  # nativeBuildInputs is usually what you want -- tools you need to run
+  nativeBuildInputs = with pkgs; [
+    nixpkgs-fmt
+    rnix-lsp
+
+    R radianWrapper
+    rPackages.ggplot2 rPackages.pacman
+    rPackages.dplyr
+#    python3
+#    python3Packages.ipython
+#    python3Packages.ipykernel
+#    python3Packages.scikit-learn
+#    python3Packages.matplotlib
+  ];
+}
